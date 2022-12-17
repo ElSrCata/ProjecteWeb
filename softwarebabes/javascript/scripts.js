c24f52366f35007596c44f6d25ae7647213f9126
ElSrCata <92938293+ElSrCata@users.noreply.github.com>
2022-12-17 12:42:07 +0100

    Initial commit

---
+++ softwarebabes/javascript/scripts.js
@@ -0,0 +1,9 @@
+$('.toggle').click(function() {
+
+    $('.formulari').animate({
+        height: "togle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity: 'toggle'
+    }, "slow");
+});
